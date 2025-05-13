cb8f3900a3bdba90098aef70e9e2b9c8c5930d11
tuchinanna <a.tuchina.anna@yandex.com>
2025-05-13 08:35:12 +0300

    delete using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
-using namespace std;
-
 int main() {
-    string name;
-    cin >> name;
-    cout << "Hello world from " << name << endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
 }
