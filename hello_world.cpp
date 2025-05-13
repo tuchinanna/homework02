ec69cef13826cb0334a81f9ebc104e59305c7ef0
tuchinanna <a.tuchina.anna@yandex.com>
2025-05-13 08:48:42 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,10 @@
+// Подключение необходимых файлов
 #include <iostream>
 #include <string>
 int main() {
+    // Создание строки и запрос ввода от пользователя
     std::string name;
     std::cin >> name;
+    // Вывод сообщения на экран
     std::cout << "Hello world from " << name << std::endl;
 }
